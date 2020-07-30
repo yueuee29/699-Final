52e235af14ba6973685d3ef10d856b3b6a7108eb
yueuee29 <55564680+yueuee29@users.noreply.github.com>
2020-07-30 23:16:30 +0800

    Add files via upload

---
+++ api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+import requests 
+import pandas as pd
+import json
+from pandas.io.json import json_normalize
+#desired_records = 103348
+key = 'listings'
+URL = 'http://marketcheck-prod.apigee.net/v2/search/car/recents?api_key=WxOyopqFPORAeROOcf3HBUH7Cc3qNuXk&dealer_id=1080226&last_seen_range=20200301-20200601&rows=50'
+
+reponse = requests.get(URL)
+json_object = reponse.json()
+
+df = pd.DataFrame(json_normalize(json_object[key]))
+print(df)
+
+df.to_csv('newCSV.csv')
+
